30b3d323090a57aee4f8e1fff86c09269a94bd74
TopsCode <kailash.solanki@tops-int.com>
2019-09-07 17:56:13 +0530

    Create AssignmentExample.c

---
+++ C/AssignmentExample.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+	int a,b;
+	clrscr();
+	a = 53;
+	printf("\n\t Value of A : %d",a);       // 53
+	b = a;	// Interchange of value using assignment
+	printf("\n\n\t Value of B : %d",b);    // 53
+	getch();
+}
