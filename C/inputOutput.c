95ba12057ba0af180953041f2298c6fe6d1619d8
TopsCode <kailash.solanki@tops-int.com>
2019-09-07 14:22:26 +0530

    Create inputOutput.c

---
+++ C/inputOutput.c
@@ -0,0 +1,10 @@
+#include < stdio.h > // Inserts stdio.h header file into the Programe
+ 
+void main ( ) // Beginning of main function. 
+{ 
+
+	char  in; // character declaration of variable in. 
+	printf (“please enter one character”); // message to user 
+	in = getchar( ) ; // assign the keyboard input value to in. 
+	putchar(in); // out put ‘in’ value to standard screen. 
+} 
