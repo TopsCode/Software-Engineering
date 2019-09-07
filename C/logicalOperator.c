ba0450bbd1290112408dcc55d179a234923d6c4b
TopsCode <kailash.solanki@tops-int.com>
2019-09-07 18:17:38 +0530

    Create logicalOperator.c

---
+++ C/logicalOperator.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main() {
+
+   int a = 5;
+   int b = 20;
+   int c ;
+
+   if ( a && b ) {
+      printf("Line 1 - Condition is true\n" );
+   }
+	
+   if ( a || b ) {
+      printf("Line 2 - Condition is true\n" );
+   }
+   
+   /* lets change the value of  a and b */
+   a = 0;
+   b = 10;
+	
+   if ( a && b ) {
+      printf("Line 3 - Condition is true\n" );
+   } else {
+      printf("Line 3 - Condition is not true\n" );
+   }
+	
+   if ( !(a && b) ) {
+      printf("Line 4 - Condition is true\n" );
+   }
+   getch();
+	
+}
