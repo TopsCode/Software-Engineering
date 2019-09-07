2a852ec163fef046fae58c869412c2ad8fd06168
TopsCode <kailash.solanki@tops-int.com>
2019-09-07 18:28:24 +0530

    Create conditionalOperator.c

---
+++ C/conditionalOperator.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+	#include <conio.h>
+	void main()
+	{	
+		int a, b=3;	
+		clrscr();	
+		a = 5;	
+		printf("\n\n A is less than B ? ");
+	(a<b)>printf(“Yes”):printf(“No”);
+	
+		getch();
+	}
