b8a1e49bd944a0857dc39a1a23dd5ad56428df61
TopsCode <kailash.solanki@tops-int.com>
2019-09-09 11:37:49 +0530

    Create expression1.c

---
+++ C/expression1.c
@@ -0,0 +1,21 @@
+#incude<stdio.h>
+#include<conio.h>
+
+void main () 
+{ 
+float a, b, c x, y, z; 
+
+a = 9; 
+b = 12; 
+c = 3; 
+
+x = a – b / 3 + c * 2 – 1; 
+y = a – b / (3 + c) * (2 – 1); 
+z = a – ( b / (3 + c) * 2) – 1; 
+
+printf (“x = %fn”,x); 
+printf (“y = %fn”,y); 
+printf (“z = %fn”,z); 
+
+getch();
+} 
