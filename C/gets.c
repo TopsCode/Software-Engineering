0be11c22cda57112696a4b40189668fe1b00a669
TopsCode <kailash.solanki@tops-int.com>
2019-09-09 17:01:12 +0530

    Create gets.c

---
+++ C/gets.c
@@ -0,0 +1,14 @@
+# include < stdio.h > 
+
+void main ( ) 
+{ 
+	char s [80]; 
+	
+	printf (“Type a string less than 80 characters:”); 
+	
+	gets (s); 
+	
+	printf (“The string types is:”); 
+	
+	puts(s); 
+}  
