36115478f4e08e2c6df25b5bdff4a748ae5bc86d
TopsCode <kailash.solanki@tops-int.com>
2019-09-09 12:05:42 +0530

    Create NestedIfElse.c

---
+++ C/NestedIfElse.c
@@ -0,0 +1,24 @@
+void main()
+{
+ int a,b,c;
+  clrscr();
+  printf("Enter the vlaue of A:-");
+  scanf("%d",&a);
+  printf("Enter the value of B:-");
+  scanf("%d",&b);
+  printf("Enter the vlaue of C:-");
+  scanf("%d",&c);
+  printf("Your Largest Value is :-");
+if(a>b)
+{
+	if(a>c){
+		printf("a is grater");
+	}else{
+		printf("c is grater")
+	}
+}else if(b>c){
+	printf("b is grater");
+}else{
+	printf("c is grater");
+}
+getch();
