0853064aa19dcab7d8a87e96b290928ce87d49e2
TopsCode <kailash.solanki@tops-int.com>
2019-09-09 12:07:27 +0530

    Create ifElseLadder.c

---
+++ C/ifElseLadder.c
@@ -0,0 +1,14 @@
+main(){
+  int i = 2;
+  if(i == 0){
+    printf(" i == 0. \n");
+  }
+   
+  else if(i == 1){
+    printf(" i == 1. \n");
+  }
+   
+  else if(i == 2){
+    printf(" i == 2. \n");
+   }
+}
