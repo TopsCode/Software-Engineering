ad4e2ee74a1d6f30590a72c2daa2d9affc6abacd
TopsCode <kailash.solanki@tops-int.com>
2019-09-10 17:39:36 +0530

    Create globalVariable.cpp

---
+++ c++/variable/globalVariable.cpp
@@ -0,0 +1,11 @@
+  #include <iostream.h>
+  
+  int x,y,z;   //Global Variables
+  float a,b,c; //Global Variables
+  
+  void main( )
+  {
+    int s,u;   //Local Variables
+    float w,q; //Local Variables
+    
+  }
