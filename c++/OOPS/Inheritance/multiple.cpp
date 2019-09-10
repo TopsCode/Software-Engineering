d4ede23aa3e0e022751a1889ac7dac5c6ae51074
TopsCode <kailash.solanki@tops-int.com>
2019-09-10 18:05:09 +0530

    Create multiple.cpp

---
+++ c++/OOPS/Inheritance/multiple.cpp
@@ -0,0 +1,52 @@
+#include<iostream.h>
+#include<conio.h>
+ 
+class student
+{
+    protected:
+    	int rno,m1,m2;
+    public:
+              	void get()
+              	{
+                            cout<<"Enter the Roll no :";
+                            cin>>rno;
+                            cout<<"Enter the two marks   :";
+                            cin>>m1>>m2;
+              	}
+};
+class sports
+{
+    protected:
+       	int sm;             // sm = Sports mark
+    public:
+                	void getsm()
+	{
+                 	        cout<<"\nEnter the sports mark :";
+                            cin>>sm;
+ 
+              	}
+};   
+
+class statement:public student,public sports
+{
+    int tot,avg;
+    public:
+    	void display()
+              	{
+                 	     tot=(m1+m2+sm);
+                 	     avg=tot/3;
+                 	     cout<<"\n\n\tRoll No: "<<rno<<"\n\tTotal: "<<tot;
+	     cout<<"\n\tAverage    : "<<avg;
+              	}
+};
+void main()
+{
+   clrscr();
+   statement obj;
+   obj.get();
+   obj.getsm();
+   obj.display();
+   getch();
+}
+
+
