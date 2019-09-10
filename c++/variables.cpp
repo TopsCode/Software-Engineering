c9fe9e8dc4ed7c63cf9584d47890c9dfcf8fa43a
TopsCode <kailash.solanki@tops-int.com>
2019-09-10 17:34:49 +0530

    Create variables.cpp

---
+++ c++/variables.cpp
@@ -0,0 +1,20 @@
+// operating with variables
+
+#include <iostream.h>
+int main ()
+{
+// declaring variables:
+int a, b;
+int result;
+
+// process:
+a = 5;
+b = 2;
+a = a + 1;
+result = a - b;
+// print out the result:
+cout << result;
+
+// terminate the program:
+return 0;
+}
