354b09b032379f748277548d9c2d12ec568ac655
TopsCode <kailash.solanki@tops-int.com>
2019-09-10 17:47:16 +0530

    Create typeCasting.cpp

---
+++ c++/variable/typeCasting.cpp
@@ -0,0 +1,16 @@
+#include<conio.h>
+#include <iostream.h>
+
+void main()
+{
+  int a;
+  float b,c;
+
+	cout<< “Enter the value of a:”;
+  cin>>a;
+	cout<< “\n Enter the value of b:”;
+  cin>>b;	
+	c = float(a)+b;
+	cout<<”\n The value of c is:”<<c;
+  
+ }
