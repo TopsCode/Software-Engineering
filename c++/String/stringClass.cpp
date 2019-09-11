8cf72d02c4889d59db3e17d28766f43df046ddcf
TopsCode <kailash.solanki@tops-int.com>
2019-09-11 11:10:44 +0530

    Create stringClass.cpp

---
+++ c++/String/stringClass.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+int main ()
+{
+   string str1 = "Hello";
+   string str2 = "World";
+   string str3;
+   int  len ;
+
+   // copy str1 into str3
+   str3 = str1;
+   cout << "str3 : " << str3 << endl;
+   // concatenates str1 and str2
+   str3 = str1 + str2;
+   cout << "str1 + str2 : " << str3 << endl;
+   // total lenghth of str3 after concatenation
+   len = str3.size();
+   cout << "str3.size() :  " << len << endl;
+}
