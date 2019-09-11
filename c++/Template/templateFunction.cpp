9f78e62f447593f7b5292c87dc7dc446add20e63
TopsCode <kailash.solanki@tops-int.com>
2019-09-11 11:14:48 +0530

    Create templateFunction.cpp

---
+++ c++/Template/templateFunction.cpp
@@ -0,0 +1,36 @@
+#include <iostream.h>
+
+template <class T, int N>
+class mysequence
+{
+      T memblock [N];
+      public:
+      void setmember (int x, T value);
+      T getmember (int x);
+};
+
+template <class T, int N>
+void mysequence<T,N>::setmember (int x, T value) 
+{
+      memblock[x]=value;
+}
+         
+template <class T, int N>
+T mysequence<T,N>::getmember (int x)
+{
+	  return memblock[x];
+}
+
+int main ()
+{
+      clrscr();
+      mysequence <int,5> myints;
+      mysequence <double,5> myfloats;
+      myints.setmember (0,100);
+      myfloats.setmember (3,3.1416);
+      cout << myints.getmember(0) << '\n';
+      cout << myfloats.getmember(3) << '\n';
+      getch();
+      return 0;
+}
+
