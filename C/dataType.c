ebce8afd7bf30b4f7937f74c0e97877460e100f4
TopsCode <kailash.solanki@tops-int.com>
2019-09-19 18:54:56 +0530

    Create dataType.c

---
+++ C/dataType.c
@@ -0,0 +1,21 @@
+#include<conio.h>
+#include <stdio.h> 
+int main() 
+{ 
+    int a = 1; 
+    char b ='G'; 
+    double c = 3.14; 
+    printf("Hello World!\n"); 
+  
+    //printing the variables defined above along with their sizes 
+    printf("Hello! I am a character. My value is %c, b); 
+    
+  
+    printf("Hello! I am an integer. My value is %d  ",a);
+           
+    printf("Hello! I am a double floating point variable. My value is %lf",c); 
+    
+    printf("Bye! See you soon. :)\n"); 
+    getch()
+    return 0; 
+} 
