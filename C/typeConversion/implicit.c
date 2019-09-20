416ba5ea97542edf2df27c7c2f41c38452a78a41
TopsCode <kailash.solanki@tops-int.com>
2019-09-20 13:09:49 +0530

    Create implicit.c

---
+++ C/typeConversion/implicit.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include<conio.h
+
+void main() {
+   int  num = 13;
+   char c = 'k'; /* ASCII value is 107 */
+   float sum;
+   sum = num + c;
+   printf("sum = %f\n", sum );
+} 
