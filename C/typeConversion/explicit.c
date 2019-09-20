6c635ff574f93f3ef7db58385d9ba9ed0b3e09de
TopsCode <kailash.solanki@tops-int.com>
2019-09-20 13:20:39 +0530

    Create explicit.c

---
+++ C/typeConversion/explicit.c
@@ -0,0 +1,13 @@
+#include<stdio.h> 
+#include<conio.h>
+int main() 
+{ 
+    double x = 1.2; 
+  
+    // Explicit conversion from double to int 
+    int sum = (int)x + 1; 
+  
+    printf("sum = %d", sum); 
+    getch();
+    return 0; 
+} 
