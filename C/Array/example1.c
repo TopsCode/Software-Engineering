762bd49a61a6c4a194ba2988d74b37fa8a54a2c1
TopsCode <kailash.solanki@tops-int.com>
2019-09-20 14:27:55 +0530

    Create example1.c

---
+++ C/Array/example1.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+#include<conio.h>
+
+int main() 
+{ 
+  
+    // Array declaration by initializing it with more 
+    // elements than specified size. 
+    int arr[2] = { 10, 20, 30, 40, 50 }; 
+    getch();
+    return 0; 
+} 
