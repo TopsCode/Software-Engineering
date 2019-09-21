0f8aa5df269c676b4e05a430364b01c8ee2ab601
TopsCode <kailash.solanki@tops-int.com>
2019-09-21 17:46:50 +0530

    Create thisPointer.cpp

---
+++ c++/OOPS/class/thisPointer.cpp
@@ -0,0 +1,28 @@
+#include<iostream.h>
+#include<conio.h>
+
+  
+/* local variable is same as a member's name */
+class Test 
+{ 
+private: 
+   int x; 
+public: 
+   void setX (int x) 
+   { 
+       // The 'this' pointer is used to retrieve the object's x 
+       // hidden by the local variable 'x' 
+       this->x = x; 
+   } 
+   void print() { cout << "x = " << x << endl; } 
+}; 
+  
+int main() 
+{ 
+   Test obj; 
+   int x = 20; 
+   obj.setX(x); 
+   obj.print(); 
+   getch();
+   return 0; 
+} 
