b012e3942702a7e00995c99fab4c288a541e95c5
TopsCode <kailash.solanki@tops-int.com>
2019-09-21 17:29:07 +0530

    Create class.cpp

---
+++ c++/OOPS/class/class.cpp
@@ -0,0 +1,21 @@
+#include<iostream.h>
+#include<conio.h>
+
+class Test
+{
+    
+        int data1;
+        float data2;  
+   
+        void function1()
+        {   data1 = 2;  } 
+        
+        float function2()
+        { 
+            data2 = 3.5;
+            return data2;
+        }
+   };
+int main()
+{
+    Test o1, o2;
